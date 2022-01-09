e5f326cb0290a030850273945d1121a21debb829
jukanntenn <jukanntenn@outlook.com>
2022-01-09 15:38:37 +0800

    fix naming

--- ch2/Exercise2.3/popcount_test.go
+++ ch2/Exercise2.3/popcount_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 // BenchmarkPopcount-8             1000000000               0.3038 ns/op
-func BenchmarkPopcount(b *testing.B) {
+func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		PopCount(1000)
 	}
 }
 
 // BenchmarkPopcountLoop-8         197738172                6.075 ns/op
-func BenchmarkPopcountLoop(b *testing.B) {
+func BenchmarkPopCountLoop(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		PopCountLoop(1000)
 	}
---
+++ ch2/Exercise2.4/popcount_test.go
@@ -0,0 +1,29 @@
+package popcount
+
+import (
+	"testing"
+)
+
+// BenchmarkPopcount-8             1000000000               0.3068 ns/op
+func BenchmarkPopcount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0x1234567890ABCDEF)
+	}
+}
+
+// BenchmarkPopCountShift-8        43614860                25.94 ns/op
+func BenchmarkPopCountShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountShift(0x1234567890ABCDEF)
+	}
+}
+
+func TestCorrectness(t *testing.T) {
+	tests := []uint64{0, 1, 21845, 1185589760, 0x1234567890ABCDEF}
+
+	for _, test := range tests {
+		if PopCount(test) != PopCountShift(test) {
+			t.Errorf("PopCount(%v) != PopCountShift(%v)", test, test)
+		}
+	}
+}
