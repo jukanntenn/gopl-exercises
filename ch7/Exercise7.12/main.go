5c736bab27c716d6330c143619082a788a4e766c
jukanntenn <jukanntenn@outlook.com>
2022-06-04 15:10:40 +0800

    Finished 7.12

---
+++ ch7/Exercise7.12/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+var html = template.Must(template.New("table").Parse(`
+<html>
+<body>
+<table>
+	<tr>
+		<th>Item</th>
+		<th>Price</th>
+	</tr>
+{{ range $item, $price := . }}
+	<tr>
+		<td>{{ $item }}</td>
+		<td>{{ $price }}</td>
+	</td>
+{{end}}
+</body>
+</html>
+`))
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	err := html.Execute(w, db)
+	if err != nil {
+		log.Printf("template error: %s", err)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if price, ok := db[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
